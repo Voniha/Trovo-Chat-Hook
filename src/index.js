a5085c170266addda4886fc6df9a0ea345b3c83a
Voniha <70020299+Voniha@users.noreply.github.com>
2022-10-13 15:37:23 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-// Trovo imports
-const token = require("./functions/getToken");
-const socket = require("./functions/websocket");
-
 // Program imports
 const intModule = require("./functions/interactiveModule");
 
